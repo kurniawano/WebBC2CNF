b118e5436d216d24fc35650410815dcf508509e4
kurniawano <kurniawano@ieee.org>
2015-08-20 06:52:08 +0000

    Add form html

--- bc2cnf/views.py
+++ bc2cnf/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    context={}
+    return render(request, 'bc2cnf/index.html', context)
+
+def submit(request):
+    return HttpResponse('Submitted.')
